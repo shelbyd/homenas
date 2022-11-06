c211103450f8612cb33b7c4b96cbb0504954331f
Shelby Doolittle <shelby@pinestreetlabs.com>
2022-11-06 02:02:28 -0500

    Implement get_attributes

--- src/file_system.rs
+++ src/file_system.rs
@@ -5,30 +5,54 @@ use std::{
 
 #[async_trait::async_trait]
 pub trait FileSystem {
-    async fn lookup(&self, parent: u64, name: &OsStr) -> Result<File, c_int>;
+    async fn lookup(&self, parent: u64, name: &OsStr) -> Result<Attributes, c_int>;
+    async fn get_attributes(&self, node: u64) -> Result<Attributes, c_int>;
 }
 
-pub struct File {
+pub struct Attributes {
     pub created_at: SystemTime,
     pub node_id: u64,
     pub size: u64,
+}
+
+impl Attributes {
+    fn file() -> Self {
+        Attributes {
+            created_at: std::time::UNIX_EPOCH,
+            node_id: 2,
+            size: 13,
+        }
+    }
+
+    fn dir() -> Self {
+        Attributes {
+            created_at: std::time::UNIX_EPOCH,
+            node_id: 1,
+            size: 0,
+        }
+    }
 }
 
 pub struct Main;
 
 #[async_trait::async_trait]
 impl FileSystem for Main {
-    async fn lookup(&self, parent: u64, name: &OsStr) -> Result<File, c_int> {
+    async fn lookup(&self, parent: u64, name: &OsStr) -> Result<Attributes, c_int> {
         log::info!("lookup(parent, name): {:?}", (parent, name.to_str()));
 
         if parent == 1 && name.to_str() == Some("hello.txt") {
-            return Ok(File {
-                created_at: std::time::UNIX_EPOCH,
-                node_id: 2,
-                size: 13,
-            });
+            return Ok(Attributes::file());
         }
 
         Err(libc::ENOENT)
+    }
+
+    async fn get_attributes(&self, node: u64) -> Result<Attributes, c_int> {
+        log::info!("get_attributes(node): {:?}", node);
+
+        match node {
+            1 => Ok(Attributes::dir()),
+            _ => Err(libc::ENOENT),
+        }
     }
 }
--- src/fuse/unix.rs
+++ src/fuse/unix.rs
@@ -8,6 +8,15 @@ impl<F> UnixWrapper<F> {
     pub fn new(f: F) -> Self {
         UnixWrapper(Arc::new(f))
     }
+
+    fn inner<G, Fut>(&self, g: G)
+    where
+        G: FnOnce(Arc<F>) -> Fut,
+        Fut: core::future::Future<Output = ()> + Send + 'static,
+    {
+        let inner = Arc::clone(&self.0);
+        tokio::spawn(g(inner));
+    }
 }
 
 impl<F> fuse::Filesystem for UnixWrapper<F>
@@ -16,38 +25,48 @@ where
 {
     fn lookup(&mut self, _req: &Request, parent: u64, name: &OsStr, reply: ReplyEntry) {
         let name = name.to_owned();
-        let wrapped = Arc::clone(&self.0);
-        ::tokio::spawn(async move {
-            let result = wrapped.lookup(parent, &name).await;
-            match result {
-                Ok(file) => {
-                    let passed = file
-                        .created_at
-                        .duration_since(std::time::UNIX_EPOCH)
-                        .expect("should always be after epoch");
-                    let create_time =
-                        Timespec::new(passed.as_secs() as i64, passed.subsec_nanos() as i32);
-                    // TODO(shelbyd): Don't hardcode these values.
-                    let attr = FileAttr {
-                        ino: file.node_id,
-                        size: file.size,
-                        blocks: 1,
-                        atime: create_time,
-                        mtime: create_time,
-                        ctime: create_time,
-                        crtime: create_time,
-                        kind: FileType::RegularFile,
-                        perm: 0o644,
-                        nlink: 1,
-                        uid: 501,
-                        gid: 20,
-                        rdev: 0,
-                        flags: 0,
-                    };
-                    reply.entry(&::time::Timespec::new(1, 0), &attr, 0);
-                }
+
+        self.inner(|inner| async move {
+            match inner.lookup(parent, &name).await {
+                Ok(file) => reply.entry(&Timespec::new(1, 0), &attr_to_unix(file), 0),
                 Err(c_int) => reply.error(c_int),
             }
         });
+    }
+
+    fn getattr(&mut self, _req: &Request, ino: u64, reply: ReplyAttr) {
+        self.inner(|inner| async move {
+            let attrs = match inner.get_attributes(ino).await {
+                Err(e) => return reply.error(e),
+                Ok(v) => v,
+            };
+            reply.attr(&Timespec::new(1, 0), &attr_to_unix(attrs));
+        });
+    }
+}
+
+fn attr_to_unix(attrs: crate::file_system::Attributes) -> FileAttr {
+    let passed = attrs
+        .created_at
+        .duration_since(std::time::UNIX_EPOCH)
+        .expect("should always be after epoch");
+    let create_time = Timespec::new(passed.as_secs() as i64, passed.subsec_nanos() as i32);
+
+    // TODO(shelbyd): Don't hardcode these values.
+    FileAttr {
+        ino: attrs.node_id,
+        size: attrs.size,
+        blocks: attrs.size / 256,
+        atime: create_time,
+        mtime: create_time,
+        ctime: create_time,
+        crtime: create_time,
+        kind: FileType::RegularFile,
+        perm: 0o644,
+        nlink: 1,
+        uid: 501,
+        gid: 20,
+        rdev: 0,
+        flags: 0,
     }
 }
